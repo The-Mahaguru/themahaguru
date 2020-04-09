559c44a01153450a185bf705dba5b33af0e8b175
Vaibhav Saini <sainivaibhav19@gmail.com>
2020-04-09 16:31:28 +0530

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>TheMahaguru-Learning Master</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta content="" name="keywords">
+	<meta content="" name="description">
+
+	<!-- Favicons -->
+	<link href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqXeoOjg_zeR7fnC3MxD0ePNg1e9bXGt63g7Mvx9N8hmybOh9H&s" rel="icon">
+	<link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+	<!-- Bootstrap CSS File -->
+	<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Libraries CSS Files -->
+	<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="lib/animate/animate.min.css" rel="stylesheet">
+	<link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+	<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+	<link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+	<!-- Main Stylesheet File -->
+	<link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body id="body">
+
+	<!--==========================
+    Top Bar
+  ============================-->
+	<section id="topbar" class="d-none d-lg-block">
+		<div class="container clearfix">
+			<div class="contact-info float-left">
+				<i class="fa fa-envelope-o"></i> <a href="mailto:contact@example.com">indiathemahaguru@gmail.com</a>
+				<i class="fa fa-phone"></i> +91 9844709645
+			</div>
+			<div class="social-links float-right">
+				<a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+				<a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+				<a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+				<a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+				<a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+			</div>
+		</div>
+	</section>
+
+	<!--==========================
+    Header
+  ============================-->
+	<header id="header">
+		<div class="container">
+
+			<div id="logo" class="pull-left">
+				<!-- <h1><a href="#body" class="scrollto">the<span>Mahaguru</span></a></h1> -->
+				<!-- Uncomment below if you prefer to use an image logo -->
+				<a href="#body"><img src="img/icon.png" alt="" title="" /></a>
+			</div>
+
+			<nav id="nav-menu-container">
+				<ul class="nav-menu">
+					<li class="menu-active"><a href="index.html">Home</a></li>
+					<li><a href="#">Training</a></li>
+					<li><a href="#services">Services</a></li>
+					<li><a href="#portfolio">Blog</a></li>
+					<li class="menu-has-children"><a href="">Log In</a>
+						<ul>
+							<li><a href="#">Students</a></li>
+							<li><a href="#">Parents</a></li>
+							<li><a href="#">Teachers</a></li>
+							<li><a href="#">Admin</a></li>
+							<li><a href="#">Support Staff</a></li>
+						</ul>
+					</li>
+					<li><a href="#team">FAQs</a></li>
+					<li><a href="#team">About Us</a></li>
+					<li><a href="#contact">Contact Us</a></li>
+				</ul>
+			</nav><!-- #nav-menu-container -->
+		</div>
+	</header><!-- #header -->
+
+	<div class="card text-center" style="width: 60%;margin-left: 20vw;margin-top: 35px">
+		<div class="card-header" style="font-size: 1.5rem">
+			<b>AI SOLUTIONS</b>
+		</div>
+		<div class="card-body">
+			<img class="card-img-top" src="http://www.themahaguru.in/wp-content/uploads/2019/08/ai4.jpeg" alt="Card image cap" style="height: 20rem;width: 90%">
+			<h5 class="card-title"></h5>
+			<p class="card-text" style="letter-spacing: 1px">In computer science, artificial intelligence(AI), sometimes called machine is intelligence demonstrated by machines, <br>in contrast to the natural intelligence displayed by humans. The term “artificial intelligence” is often <br>used to describe machines (or computers) that mimic “cognitive” functions that<br> humans associate with the human mind, such as “learning” and “problem solving”.</p>
+			<a href="#" class="btn btn-primary">LEAVE A REPLY</a>
+		</div>
+		<div class="card-footer text-muted">
+			2 days ago
+		</div>
+	</div>
+	<br><br>
+
+	<!--==========================
+    Footer
+  ============================-->
+	<footer id="footer">
+		<div class="container">
+			<div class="copyright">
+				&copy; Copyright <strong>the Mahaguru</strong>. All Rights Reserved
+			</div>
+			<div class="credits">
+				<!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Reveal
+        -->
+				Designed by <a href="https://bootstrapmade.com/">Jacklean Venture</a>
+			</div>
+		</div>
+	</footer><!-- #footer -->
+
+	<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+	<!-- JavaScript Libraries -->
+	<script src="lib/jquery/jquery.min.js"></script>
+	<script src="lib/jquery/jquery-migrate.min.js"></script>
+	<script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="lib/easing/easing.min.js"></script>
+	<script src="lib/superfish/hoverIntent.js"></script>
+	<script src="lib/superfish/superfish.min.js"></script>
+	<script src="lib/wow/wow.min.js"></script>
+	<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+	<script src="lib/magnific-popup/magnific-popup.min.js"></script>
+	<script src="lib/sticky/sticky.js"></script>
+
+	<!-- Contact Form JavaScript File -->
+	<script src="contactform/contactform.js"></script>
+
+	<!-- Template Main Javascript File -->
+	<script src="js/main.js"></script>
+
+</body>
+
+</html>
